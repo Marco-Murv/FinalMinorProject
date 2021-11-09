4d04f91a05aaedd9e3b6ed8826bd720d2c4d1bfd
Simon Van Hulle <simonvanhulle01@gmail.com>
2021-11-09 17:14:47 +0100

    Added fitness function

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -10,9 +10,12 @@ from ase import Atoms
 from ase.calculators.lj import LennardJones
 from ase.ga.startgenerator import StartGenerator
 from ase.optimize import LBFGS
+import sys
 
 import mating
 import mutators
+
+import time
 
 
 def generate_cluster(cluster_size, radius) -> Atoms:
@@ -26,7 +29,7 @@ def generate_cluster(cluster_size, radius) -> Atoms:
     return new_cluster
 
 
-def generate_population(popul_size, cluster_size, radius):
+def generate_population(popul_size, cluster_size, radius) -> [Atoms]:
     """Generate initial population
     """
     return [generate_cluster(cluster_size, radius) for i in range(popul_size)]
@@ -38,7 +41,30 @@ def optimise_local(population, calc, optimiser) -> None:
     [atoms.set_calculator(calc) for atoms in population]
     [optimiser(atoms, logfile=None).run() for atoms in population]
 
-    return 
+    return
+
+
+def fitness(population, func="exponential"):
+    """Calculate the fitness of the clusters in the population
+    """
+    # Normalise the energies
+    energy = np.array([atoms.get_potential_energy() for atoms in population])
+    normalised_energy = (energy - np.min(energy)) / \
+        (np.max(energy) - np.min(energy))
+
+    if func == "exponential":
+        alpha = 3  # How general is this value?
+        return np.exp(- alpha * normalised_energy)
+
+    elif func == "linear":
+        return 1 - 0.7 * normalised_energy
+
+    elif func == "hyperbolic":
+        return 0.5 * (1 - np.tanh(2 * energy - 1))
+
+    else:
+        print(f"'{func}' is not a valid fitness function. Using default")
+        return fitness(population)
 
 
 def main() -> None:
@@ -47,10 +73,10 @@ def main() -> None:
     delta_energy_threshold = 0.1  # TODO: Change this
     local_optimiser = LBFGS
     children_perc = 0.8  # TODO: Change later
-    fitness_func = None
+    fitness_func = "exponential"
     cluster_radius = 2  # Angstroms TODO: Change this
     cluster_size = 3
-    popul_size = 2
+    popul_size = 20
 
     # Make local optimisation calculator
     calc = LennardJones(sigma=1.0, epsilon=1.0)  # TODO: Change parameters
@@ -60,7 +86,7 @@ def main() -> None:
 
     optimise_local(population, calc, local_optimiser)
     # Determine fitness
-    population_fitness = None
+    population_fitness = fitness(population, fitness_func)
 
     # while not done (max iterations / optimum not changing)
     for i in range(10):
@@ -79,7 +105,7 @@ def main() -> None:
         population += children + mutants
 
         # Natural selection
-        population_fitness = None
+        population_fitness = fitness(population, fitness_func)
         # Sort based on fitness, check if not too close (DeltaEnergy) and select popul_size best
         population = population
 
@@ -90,10 +116,3 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
-    # atoms = generate_cluster(3, 3)
-    # atoms.set_calculator(LennardJones())
-    # print(atoms.get_potential_energy())
-    # dyn = LBFGS(atoms)
-    # dyn.run()
-    # print(atoms.get_potential_energy())
