d7046e6ea340ca7dfba3b1f0707afb5c70d80be4
Einar de Gruyl <einardegruyl@gmail.com>
2021-11-16 20:48:27 +0100

    Refactored code into a class for greater usability
    - Added documentation to main functions
    - Save all (accepted) intermediate results

--- basin_hopping/basin_hopping.py
+++ basin_hopping/basin_hopping.py
@@ -2,148 +2,182 @@
 from mpi4py import MPI
 import numpy as np
 import math
+from typing import List, Tuple
 
 from ase import Atoms
-from ase.optimize import LBFGS
 from ase.calculators.lj import LennardJones
+from ase.optimize import LBFGS
+from ase.optimize.optimize import Optimizer
+from ase.units import kB
+from ase.visualize import view
 
 # Initialize MPI related constants
 COMM = MPI.COMM_WORLD
 SIZE = COMM.Get_size()
 RANK = COMM.Get_rank()
 
-# Constants
-# Max iterations
-MAX_ITERATIONS = 5000
-STOP_ITERATIONS = 100
-# Cluster size
-N = 3
-# Cluster accept rate
-ACCEPT_RATE = 0.5
-# Step size adjustment interval
-INTERVAL = 10
-# Stepsize adjustment factor
-FACTOR = 0.9
-# Radius of initial configuration
-R = 2.5 # Å
-MAX_R = 3.0 # Å
-# Temperature for metropolis acceptance criterion
-BOLTZMANN = 8.61733034e-5 # eV/K
-T = 100 * BOLTZMANN
-
-# Global variables
-stepSize = 0.5
-nAccept = 1.0
-nTotal = 1.0
-
-# Class for storing cluster configurations
-class Cluster:
-    def __init__(self, positions, potential_energy) -> None:
-        self.positions = positions
-        self.potential_energy = potential_energy
+class CubeConstraint:
+    """Constrain atoms inside of a cube."""
+    def __init__(self, radius) -> None:
+        self.radius = radius
     
-    def __str__(self) -> str:
-        return f"Cluster(positions={self.positions}, potential_energy={self.potential_energy})"
+    def adjust_positions(self, atoms, positions):
+        positions[:] = np.clip(positions, -self.radius, self.radius)
+    
+    def adjust_forces(self, atoms, forces):
+        pass
 
-def generateConfiguration():
+class BasinHopping:
+    """
+    Implementation of the basin hopping algorithm described by Wales, D. J., & Doye, J. P. K. (1997).
+
+    Parameters
+    ----------
+    atoms: Atoms object
+        The atoms object to perform basin hopping on.
+    optimizer: Optimizer object, optional
+        The optimizer used for local optimization.
+    optimizer_logfile: string, None, optional
+        The logfile to write the output of the optimizer to.
+        Use '-' for stdout
+        If None (default), the output of the optimizer is discarded.
+    temperature: float, optional
+        The temperature parameter for the Metropolis acceptance criterion.
+    step_size: float, optional
+        The initial value of the step size.
+    accept_rate: float, optional
+        The desired step acceptance rate.
+    factor: float, optional
+        The factor to multiply and divide the step size by.
+    interval: float, optional
+        The interval for how often to update the step size.
+    """
+    def __init__(self,
+                 atoms: Atoms,
+                 optimizer: Optimizer=LBFGS,
+                 optimizer_logfile: str=None,
+                 temperature: float=100*kB,
+                 step_size: float=0.5,
+                 accept_rate: float=0.5,
+                 factor: float=0.9,
+                 interval: int=50) -> None:
+        self.atoms = atoms
+        self.optimizer = optimizer
+        self.optimizer_logfile = optimizer_logfile
+        self.temperature = temperature
+        self.step_size = step_size
+        self.accept_rate = accept_rate
+        self.step_size_factor = factor
+        self.step_size_interval = interval
+        self.nTotal = self.nAccept = 0
+        # Minima
+        self.min_potential_energy = self.atoms.get_potential_energy()
+        self.min_atoms = self.atoms.copy()
+        self.minima = [(self.min_potential_energy, self.min_atoms)]
+    
+    def run(self, max_steps: int=5000, stop_steps: int=None, verbose: bool=True) -> Tuple[float, Atoms, List[Tuple[float, List[List[float]]]]]:
+        """
+        Run the basin hopping algorithm.
+
+        Parameters
+        ----------
+        max_steps: int, optional
+            The maximum number of steps the algorithm will take.
+        stop_steps: int, optional
+            The number of steps, without there being a new minimum, the algorithm will take before stopping.
+            If None (default), the algorithm will run for the maximum number of steps.
+        verbose: bool, optional
+            Print information about each step.
+        """
+        stop_step_count = 0
+        old_potential_energy = self.min_potential_energy
+        if verbose: print("{:s} {:>5s} {:>16s} {:>8s}".format(" "*13, "Step", "Energy", "Accept"))
+        for i in range(max_steps):
+            old_positions = self.atoms.get_positions()
+            # Displace atoms
+            self.displace_atoms()
+            # Get the potential energy
+            new_potential_energy = self.get_potential_energy()
+            # Check if new global minimum was found
+            if new_potential_energy < self.min_potential_energy:
+                self.min_atoms = self.atoms.copy()
+                self.min_potential_energy = new_potential_energy
+                stop_step_count = 0
+            else:
+                stop_step_count += 1
+            # Acceptance criterion
+            accept = self.accept(old_potential_energy, new_potential_energy)
+            # Step size adjustment
+            if accept: self.nAccept += 1
+            self.nTotal += 1
+            if self.nTotal % self.step_size_interval: self.adjust_step_size()
+            # Log step
+            if verbose: print(f"BasinHopping: {i:5d} {new_potential_energy:15.6f}* {str(accept):>8s}")
+            # Set old values and save configuration
+            if accept:
+                old_potential_energy = new_potential_energy
+                self.minima.append((new_potential_energy, self.atoms.get_positions()))
+            else:
+                self.atoms.set_positions(old_positions)
+            # Stop condition
+            if stop_steps is not None and stop_step_count >= stop_steps: break
+        
+        print(f"Stopped at iteration {i}.")
+        return self.min_potential_energy, self.min_atoms, self.minima
+        
+    
+    def displace_atoms(self) -> None:
+        dX = np.random.uniform(-self.step_size, self.step_size, (len(self.atoms), 3))
+        self.atoms.translate(dX)
+    
+    def get_potential_energy(self) -> float:
+        # Ignore divide by zero errors
+        with np.errstate(divide='ignore', invalid='ignore'):
+            self.optimizer(self.atoms, logfile=self.optimizer_logfile).run(steps=50)
+            return self.atoms.get_potential_energy()
+    
+    def accept(self, old_potential_energy: float, new_potential_energy: float) -> bool:
+        """Metropolis acceptance criterion.
+        If ΔE <= 0, always accept.
+        If ΔE > 0, sometimes accept.
+        """
+        dE = new_potential_energy - old_potential_energy
+        return True if dE <= 0 else math.exp(-dE/self.temperature) >= np.random.uniform()
+    
+    def adjust_step_size(self) -> None:
+        if self.nAccept / self.nTotal > self.accept_rate:
+            self.step_size /= self.step_size_factor
+        else:
+            self.step_size *= self.step_size_factor
+
+def generateConfiguration(N: int, radius: int) -> np.ndarray:
     # Generate normally distributed points on the surface of the sphere
     XYZ = np.random.normal(size=(N, 3))
     XYZ /= np.linalg.norm(XYZ, axis=1)[:,np.newaxis]
     # Generate uniformly distributed points
-    U = R*np.cbrt(np.random.uniform(size=(N)))
+    U = radius*np.cbrt(np.random.uniform(size=(N)))
     # Calculate uniformly distributed points inside the sphere
     return XYZ*U[:,np.newaxis]
 
-def displaceConfiguration(X):
-    # Displace points uniformly
-    return np.clip(X + np.random.uniform(-stepSize, stepSize, (N, 3)), -MAX_R, MAX_R)
-
-def localMinimisation(atoms):
-    optimiser = LBFGS(atoms, logfile=None)
-    optimiser.run(steps=50)
-
-# Metropolis acceptance criterion
-def accept(dE):
-    # If ΔE <= 0, always accept
-    if dE <= 0: return True
-    # If ΔE > 0, sometimes accept
-    return math.exp(-dE/T) >= np.random.uniform()
-
-def adjustStepSize():
-    global stepSize
-    if nAccept / nTotal > ACCEPT_RATE:
-        # Too many steps are accepted, increase step size.
-        stepSize /= FACTOR
-    else:
-        # Too few steps are accepted, decrease step size.
-        stepSize *= FACTOR
-
-def basinHopping(X, verbose=False):
-    global nTotal, nAccept
-
-    atoms = Atoms(positions=X, calculator=LennardJones())
-    localMinimisation(atoms)
-    prevE = atoms.get_potential_energy()
-    minCluster = Cluster(X, prevE)
-
-    clusters = [minCluster]
-    steps_left = STOP_ITERATIONS
-
-    # Max iterations
-    for _ in range(MAX_ITERATIONS):
-        newX = displaceConfiguration(X)
-        atoms.set_positions(newX)
-        # Local minimisation
-        localMinimisation(atoms)
-        # Potential energy
-        E = atoms.get_potential_energy()
-        if E < minCluster.potential_energy:
-            print(f"New local minimum = {E}")
-            minCluster = Cluster(newX, E)
-            clusters.append(minCluster)
-            steps_left = STOP_ITERATIONS
-        else:
-            steps_left -= 1
-        if steps_left < 0:
-            break
-        dE = E - prevE
-        # Acceptance
-        acc = accept(dE)
-        # Increase step totals
-        nTotal += 1
-        nAccept += 1 if acc else 0
-        if nTotal % INTERVAL == 0: adjustStepSize()
-        # Print intermediate result
-        if verbose:
-            print(f"potential energy = {E}, accepted = {acc}, accept rate = {nAccept / nTotal}")
-        # If configuration is not accepted, continue with previous configuration
-        if not acc: continue
-        # Else, continue with new configuration
-        X = newX
-        prevE = E
-    
-    return minCluster, clusters
-
 def main():
-    # Generate initial configurations
-    data = [generateConfiguration() for _ in range(SIZE)] if RANK == 0 else None
+    atoms = Atoms(positions=generateConfiguration(N=3, radius=1), constraint=CubeConstraint(radius=1.5), calculator=LennardJones())
+    potential_energy, atoms, minima = BasinHopping(atoms=atoms).run(1000, None)
 
-    # Distribute initial configurations
-    X = COMM.scatter(data)
+    potential_energy = COMM.gather(potential_energy)
+    atoms = COMM.gather(atoms)
+    minima = COMM.gather(minima)
 
-    # Run basin hopping algorithm
-    print(f"Process {RANK} started")
-    minCluster, clusters = basinHopping(X, False)
-    print(f"Process {RANK} finished")
-
-    # Gather results
-    minClusters = COMM.gather(minCluster)
-    allClusters = COMM.gather(clusters)
-
-    # Print results
     if RANK == 0:
-        print(minClusters)
-        # print(allClusters)
+        min_potential_energy = np.min(potential_energy)
+        min_atoms = atoms[np.argmin(potential_energy)]
+        # Flatten minima
+        minima = [j for i in minima for j in i]
+        # Sort minima
+        minima = minima.sort(key=lambda x:x[0])
+        # Display global minimum
+        view(min_atoms)
+        print(f"Global minimum = {min_potential_energy}")
+        print(min_atoms.get_positions())
 
 if __name__ == "__main__":
     main()
