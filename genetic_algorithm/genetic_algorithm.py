b345dc86bcb362fe94cc208aded753ea99652ed1
Simon Van Hulle <simonvanhulle01@gmail.com>
2021-11-17 11:02:05 +0100

    Changed docstrings + First refactoring

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -3,14 +3,25 @@
 """
 Genetic algorithm for geometry optimisation of atomic clusters.
 We can add more information later.
+
+This program requires a file called `run_config.yaml` in the same directory.
+Example run_config.yaml:
+```yaml
+    children_perc: 0.8
+    cluster_radius: 2.0
+    cluster_size: 5
+    delta_energy_thr: 0.001
+    fitness_func: exponential
+    mating_method: roulette
+    max_gen: 50
+    max_no_success: 5
+    pop_size: 12
+    run_id: 1
+```
 """
 
 """NOTES
-    * TODO: Something might be wrong with the fitness values
-    * FIXME: Why are the potential energies negative? What is 0?
     * TODO: See other TODOs in the file.
-    * TODO: Collect parameter values in another file.
-    * TODO: Use less .get_potential_energy() (calculates every time)
 """
 
 
@@ -33,6 +44,7 @@ import sys
 def debug(*args, **kwargs) -> None:
     """Alias for print() function.
     This can easily be redefined to disable all output.
+
     """
     print("[DEBUG]: ", *args, **kwargs)
 
@@ -41,15 +53,14 @@ def generate_cluster(cluster_size, radius) -> Atoms:
     """Generate a random cluster with set number of atoms
     The atoms will be placed within a (radius x radius x radius) cube.
 
-    Args:
-        cluster_size (int)  : Number of atoms per cluster
-        radius (float)      : dimension of the space where atoms can be placed.
+    :param cluster_size: number of atoms per cluster
+    :param radius: dimension of the space where atoms can be placed.
+    :returns: -> new random cluster
 
-    Returns:
-        new_cluster (Atoms) : Randomly generated cluster
     """
 
     coords = np.random.uniform(-radius/2, radius/2, (cluster_size, 3)).tolist()
+
     # TODO: Can we use "mathematical dots" instead of H-atoms
     new_cluster = Atoms('H'+str(cluster_size), coords)
 
@@ -59,13 +70,11 @@ def generate_cluster(cluster_size, radius) -> Atoms:
 def generate_population(popul_size, cluster_size, radius) -> List[Atoms]:
     """Generate initial population.
 
-    Args:
-        popul_size (int)    : number of clusters in the population
-        cluster_size (int)  : number of atoms in each cluster
-        radius (float)      : dimension of the initial random clusters
+    :param popul_size: number of clusters in the population
+    :param cluster_size: number of atoms in each cluster
+    :param radius: dimension of the initial random clusters
+    :returns: -> List of clusters
 
-    Returns:
-        (List[Atoms])       : List of clusters
     """
     return [generate_cluster(cluster_size, radius) for i in range(popul_size)]
 
@@ -75,13 +84,11 @@ def optimise_local(population, calc, optimiser) -> List[Atoms]:
     are optimised and can be used after this function is called. Moreover,
     calculate and return the final optimised potential energy of the clusters.
 
-    Args:
-        population(List[Atoms]) : List of clusters to be locally optimised
-        calc (Calculator)       : ASE Calculator for potential energy (e.g. LJ)
-        optimiser (Optimiser)   : ASE Optimiser (e.g. LBFGS)
+    :param population: List of clusters to be locally optimised
+    :param calc: ASE Calculator for potential energy (e.g. LJ)
+    :param optimiser: ASE Optimiser (e.g. LBFGS)
+    :returns: -> Optimised population
 
-    Returns:
-        (List[Atoms])           : Optimised population
     """
     for cluster in population:
         cluster.calc = calc
@@ -99,14 +106,11 @@ def optimise_local(population, calc, optimiser) -> List[Atoms]:
 def fitness(population, func="exponential") -> np.ndarray:
     """Calculate the fitness of the clusters in the population
 
-    Args:
-        population(List[Atoms]) : List of clusters to calculate fitness
-        func (str)              : Fitness function
-                                    - "exponential" / "linear" / "hyperbolic"
-        optimiser (Optimiser)   : ASE Optimiser (e.g. LBFGS)
+    :param population: List of clusters to calculate fitness
+    :param func: Fitness function ("exponential" / "linear" / "hyperbolic")
+    :param optimiser: ASE Optimiser (e.g. LBFGS)
+    :returns: -> Optimised population
 
-    Returns:
-        (List[Atoms])           : Optimised population
     """
     # Normalise the energies
     energies = np.array([cluster.get_potential_energy()
@@ -133,6 +137,7 @@ def fitness(population, func="exponential") -> np.ndarray:
 def parse_args():
     """Parsing the most important parameters
     This will make it easy to run with different values (e.g. on a cluster)
+
     """
     parser = argparse.ArgumentParser(description='Genetic Algorithm PGGO')
     parser.add_argument('--cluster_size', default=10, type=int,
@@ -144,11 +149,13 @@ def parse_args():
     parser.add_argument('--mating_method', default="roulette",
                         help='Mating Method', metavar='')
     parser.add_argument('--children_perc', default=0.8, type=float,
-                        help='Fraction of the population that will have a child', metavar='')
+                        help='Fraction of opulation that will have a child',
+                        metavar='')
     parser.add_argument('--cluster_radius', default=2.0, type=float,
                         help='Dimension of initial random clusters', metavar='')
     parser.add_argument('--max_no_success', default=10, type=int,
-                        help='Consecutive generations allowed without new minimum', metavar='')
+                        help='Consecutive generations without new minimum',
+                        metavar='')
     parser.add_argument('--max_gen', default=50, type=int,
                         help='Maximum number of generations', metavar='')
     parser.add_argument('--delta_energy_thr', default=0.01, type=float,
@@ -159,15 +166,22 @@ def parse_args():
 
 
 def main() -> None:
+    """The main genetic algorithm 
+
+    """
     # np.random.seed(241)
     np.seterr(divide='raise')
 
-    # Connect to database
+    # Provide file name
+    db_file = "genetic_algorithm_results.db"
+    config_file = "run_config.yaml"
+
+    # Connect to database and get parameters
+    db_file = os.path.join(os.path.dirname(__file__), db_file)
     db = ase.db.connect('./genetic_algorithm_results.db')
 
-    # Get configuration of parameters
-    config_f_name = "./run_config.yaml"
-    with open(config_f_name) as f:
+    config_file = os.path.join(os.path.dirname(__file__), config_file)
+    with open(config_file) as f:
         conf = yaml.safe_load(os.path.expandvars(f.read()))
 
     cluster_size = conf['cluster_size']
@@ -186,78 +200,80 @@ def main() -> None:
     local_optimiser = LBFGS
 
     # Generate initial population and optimise locally
-    population = generate_population(
+    pop = generate_population(
         pop_size, cluster_size, cluster_radius)
-    energies = optimise_local(population, calc, local_optimiser)
+    energies = optimise_local(pop, calc, local_optimiser)
 
     # Determine fitness
-    population_fitness = fitness(population, fitness_func)
+    pop_fitness = fitness(pop, fitness_func)
 
     # Keep track of global minima. Initialised with random cluster
-    best_minima = [population[0]]
-    local_minima = [population[0]]
-    energies_minima = np.array(population[0].get_potential_energy())
+    best_min = [pop[0]]
+    local_min = [pop[0]]
+    energies_min = np.array(pop[0].get_potential_energy())
 
     # Keep track of iterations
     gen = 0
     gen_no_success = 0
 
     while gen_no_success < max_no_success and gen < max_gen:
-        debug(f"Generation {gen:2d} - Population size = {len(population)}")
+        debug(f"Generation {gen:2d} - Population size = {len(pop)}")
 
         # Mating - get new population
-        children = mating(population, population_fitness, children_perc, mating_method)
+        children = mating(pop, pop_fitness, children_perc, mating_method)
 
         # Mutating (Choose 1 out of 4 mutators)
-        mutants = mutators.displacement_static(population, 0.05, cluster_radius)
-        mutants += mutators.displacement_dynamic(population, 0.05, cluster_radius)
-        mutants += mutators.rotation(population, 0.05)
-        mutants += mutators.replacement(population, cluster_size, cluster_radius, 0.05)
-        mutants += mutators.mirror_shift(population, cluster_size, 0.05)
+        mutants = mutators.displacement_static(pop, 0.05, cluster_radius)
+        mutants += mutators.displacement_dynamic(pop, 0.05, cluster_radius)
+        mutants += mutators.rotation(pop, 0.05)
+        mutants += mutators.replacement(pop,cluster_size, cluster_radius, 0.05)
+        mutants += mutators.mirror_shift(pop, cluster_size, 0.05)
 
         # Local minimisation and add to population
         newborns = children + mutants
 
-
-
         energies += optimise_local(newborns, calc, local_optimiser)
 
         for i in range(len(newborns)):
-            if not np.any(np.isclose(energies_minima, energies[-(i+1)], atol=delta_energy_thr)):
-                local_minima.append(newborns[i])
-                energies_minima = np.append(energies_minima, energies[i])
+            too_close = np.isclose(
+                energies_min, energies[-(i+1)], atol=delta_energy_thr)
+            if not np.any(too_close):
+                local_min.append(newborns[i])
+                energies_min = np.append(energies_min, energies[i])
 
-        population += newborns
+        pop += newborns
 
         # Natural selection
-        population_fitness = fitness(population, fitness_func)
+        pop_fitness = fitness(pop, fitness_func)
 
         # Sort based on fitness, check if not too close (DeltaEnergy)
         # and select popul_size best
-        pop_sort_i = np.argsort(-population_fitness)
+        pop_sort_i = np.argsort(-pop_fitness)
 
-        pop_i = 0
-        new_population = [population[pop_sort_i[pop_i]]]
-        new_energies = [new_population[0].get_potential_energy()]
+        count = 0
+        new_pop = [pop[pop_sort_i[count]]]
+        new_energies = [new_pop[0].get_potential_energy()]
 
-        while len(new_population) < pop_size and pop_i < len(pop_sort_i)-1:
-            pop_i += 1
-            candidate = population[pop_sort_i[pop_i]]
+        while len(new_pop) < pop_size and count < len(pop_sort_i)-1:
+            count += 1
+            sorted_i = pop_sort_i[count]
+            candidate = pop[sorted_i]
+            cand_energy = energies[sorted_i]
 
-            if abs(candidate.get_potential_energy() - new_energies[-1]) > delta_energy_thr:
-                new_population.append(candidate)
-                new_energies.append(candidate.get_potential_energy())
+            if abs(cand_energy - new_energies[-1]) > delta_energy_thr:
+                new_pop.append(candidate)
+                new_energies.append(cand_energy)
 
-        population = new_population.copy()
+        # Store newly formed population
+        pop = new_pop.copy()
         energies = new_energies.copy()
 
-
         # Store current best
-        if energies[0] < best_minima[-1].get_potential_energy():
-            best_minima.append(population[0]) 
+        if energies[0] < best_min[-1].get_potential_energy():
+            best_min.append(pop[0])
             debug("New global minimum: ", energies[0])
 
-            gen_no_success = 0
+            gen_no_success = 0 # This is success, so set to zero.
 
         else:
             gen_no_success += 1
@@ -265,25 +281,26 @@ def main() -> None:
         gen += 1
 
     # Store / report
-    debug("All the minima we found:")
-    debug([cluster.get_potential_energy() for cluster in best_minima])
+    debug(f"Found {len(local_min)} local minima. in total")
+    debug("The evolution of the global minimum:")
+    debug([cluster.get_potential_energy() for cluster in best_min])
 
-    for cluster in local_minima:
+    for cluster in local_min:
         global_min = False
-        if cluster == best_minima[-1]:
+        if cluster == best_min[-1]:
             global_min = True
 
         last_id = db.write(cluster, global_min=global_min,
-                                pop_size=pop_size,
-                                cluster_size=cluster_size, max_gens=max_gen,
-                                max_no_success=max_no_success, run_id=run_id)
+                           pop_size=pop_size,
+                           cluster_size=cluster_size, max_gens=max_gen,
+                           max_no_success=max_no_success, run_id=run_id)
 
     # How to retrieve atoms:
     # atom_db = db.get(natoms=cluster_size, pop_size=10, ...).toatoms()
     #  view(best_minima[-1])
 
     conf['run_id'] += 1
-    with open(config_f_name, 'w') as f:
+    with open(config_file, 'w') as f:
         yaml.dump(conf, f)
 
     return
