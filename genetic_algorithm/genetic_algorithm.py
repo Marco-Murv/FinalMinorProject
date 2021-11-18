04883a78323b6d9c532aff89ece0f9f176e3a3fd
Marco <m.vanveen1997@hotmail.com>
2021-11-18 14:49:00 +0100

    Refactor the GA file

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -1,9 +1,9 @@
 #!/bin/python3
 
-
 """
 Genetic algorithm for geometry optimisation of atomic clusters.
 We can add more information later.
+
 This program requires a file called `run_config.yaml` in the same directory.
 Example run_config.yaml:
 ```yaml
@@ -18,10 +18,6 @@ Example run_config.yaml:
     pop_size: 12
     run_id: 1
 ```
-"""
-
-"""NOTES
-    * TODO: See other TODOs in the file.
 """
 
 import numpy as np
@@ -45,14 +41,16 @@ from dataclasses import dataclass
 
 
 def debug(*args, **kwargs) -> None:
-    """Alias for print() function.
+    """
+    Alias for print() function.
     This can easily be redefined to disable all output.
     """
     print("[DEBUG]: ", flush=True, *args, **kwargs)
 
 
 def config_info(config):
-    """Log the most important info to stdout.
+    """
+    Log the most important info to stdout.
     """
     timestamp = dt.now().strftime("%Y-%m-%d %H:%M:%S")
     n = 63
@@ -70,7 +68,7 @@ def config_info(config):
 
 
 @dataclass
-class Config():
+class Config:
     cluster_size: int = None
     pop_size: int = None
     fitness_func: str = None
@@ -87,12 +85,13 @@ class Config():
 
 
 def generate_cluster(cluster_size, radius) -> Atoms:
-    """Generate a random cluster with set number of atoms
+    """
+    Generate a random cluster with set number of atoms
     The atoms will be placed within a (radius x radius x radius) cube.
 
-    :param cluster_size: number of atoms per cluster
-    :param radius: dimension of the space where atoms can be placed.
-    :returns: -> new random cluster
+    @param cluster_size: number of atoms per cluster
+    @param radius: dimension of the space where atoms can be placed.
+    @return: new random cluster
     """
     coords = np.random.uniform(-radius / 2, radius / 2,
                                (cluster_size, 3)).tolist()
@@ -104,25 +103,27 @@ def generate_cluster(cluster_size, radius) -> Atoms:
 
 
 def generate_population(popul_size, cluster_size, radius) -> List[Atoms]:
-    """Generate initial population.
+    """
+    Generate initial population.
 
-    :param popul_size: number of clusters in the population
-    :param cluster_size: number of atoms in each cluster
-    :param radius: dimension of the initial random clusters
-    :returns: -> List of clusters
+    @param popul_size: number of clusters in the population
+    @param cluster_size: number of atoms in each cluster
+    @param radius: dimension of the initial random clusters
+    @return: List of clusters
     """
     return [generate_cluster(cluster_size, radius) for i in range(popul_size)]
 
 
 def optimise_local(population, calc, optimiser) -> List[Atoms]:
-    """Local optimisation of the population. The clusters in the population
+    """
+    Local optimisation of the population. The clusters in the population
     are optimised and can be used after this function is called. Moreover,
     calculate and return the final optimised potential energy of the clusters.
 
-    :param population: List of clusters to be locally optimised
-    :param calc: ASE Calculator for potential energy (e.g. LJ)
-    :param optimiser: ASE Optimiser (e.g. LBFGS)
-    :returns: -> Optimised population
+    @param population: List of clusters to be locally optimised
+    @param calc: ASE Calculator for potential energy (e.g. LJ)
+    @param optimiser: ASE Optimiser (e.g. LBFGS)
+    @returns: -> Optimised population
     """
     for cluster in population:
         cluster.calc = calc
@@ -136,11 +137,12 @@ def optimise_local(population, calc, optimiser) -> List[Atoms]:
 
 
 def fitness(energies, func="exponential") -> np.ndarray:
-    """Calculate the fitness of the clusters in the population
+    """
+    Calculate the fitness of the clusters in the population
 
-    :param energies: List of cluster energies
-    :param func: Fitness function ("exponential" / "linear" / "hyperbolic")
-    :returns: -> fitness values of population
+    @param energies: List of cluster energies
+    @param func: Fitness function ("exponential" / "linear" / "hyperbolic")
+    @return: fitness values of population
     """
     # Normalise the energies
     normalised_energies = (np.array(energies) - np.min(energies)) / (np.max(energies) - np.min(energies))
@@ -161,12 +163,13 @@ def fitness(energies, func="exponential") -> np.ndarray:
 
 
 def get_configuration(config_file):
-    """Set the parameters for this run.
+    """
+    Set the parameters for this run.
 
-    :param config_file: Filename to the yaml configuration
-    :type config_file: str
-    :return: object with all the configuration parameters
-    :rtype: Config
+    @param config_file: Filename to the yaml configuration
+    @type config_file: str
+    @return: object with all the configuration parameters
+    @rtype: Config
     """
 
     # Get parameters from config file
@@ -221,15 +224,17 @@ def get_configuration(config_file):
     return c
 
 
-def plot_EPP(lowest_energies, highest_energies, average_energies):
+def plot_EPP(lowest_energies, highest_energies, average_energies, run_id):
     """
     This function will show the EPP (Evolutionary Progress Plot) of this GA run.
 
     @param lowest_energies: list containing the minimum energy in each generation
     @param highest_energies: list containing the highest energy in each generation
     @param average_energies: list containing the average energy in each generation
+    @param run_id: the id of this GA run
     @return:
     """
+    plot_file = os.path.join(os.path.dirname(__file__), f'EPP_run_{run_id}.png')
     gens = np.arange(0, len(lowest_energies))
 
     plt.figure(1)
@@ -240,6 +245,7 @@ def plot_EPP(lowest_energies, highest_energies, average_energies):
     plt.title("The lowest, highest, and average energy in each generation")
     plt.xlabel("generations")
     plt.ylabel("energy")
+    plt.savefig(plot_file)
     plt.show()
 
     return 0
@@ -307,10 +313,56 @@ def natural_selection_step(pop, energies, pop_fitness, pop_size, dE_thr, fitness
     return pop, energies, pop_fitness
 
 
-def genetic_algorithm() -> None:
-    """The main genetic algorithm 
+def store_local_minima(newborns, energies, local_min, energies_min, dE_thr):
     """
-    # np.random.seed(241)
+    Stores the new local minima from the newborns list into local_min list if they differ sufficiently from
+    previously found local minima.
+
+    @param newborns: list of new cluster with new local minima
+    @param energies: energies of the newborns clusters
+    @param local_min: previously found local minima clusters
+    @param energies_min: energies of previously found local minima
+    @param dE_thr: minimum energy threshold for different local minima
+    @return: updated list containing new local minima and the corresponding energies
+    """
+
+    for i in range(len(newborns)):
+        too_close = np.isclose(energies_min, energies[-(i + 1)], atol=dE_thr)
+        if not np.any(too_close):
+            local_min.append(newborns[i])
+            energies_min = np.append(energies_min, energies[i])
+
+    return local_min, energies_min
+
+
+def store_results_database(global_min, local_min, db, c):
+    """
+    Writes GA results to the database.
+
+    @param global_min: the global minimum cluster
+    @param local_min: list of all local minima found
+    @param db: the database to write to
+    @param c: the configuration information of the GA run
+    @return: exit code 0
+    """
+
+    for cluster in local_min:
+        is_global_min = False
+        if cluster == global_min:
+            is_global_min = True
+
+        last_id = db.write(cluster, global_min=is_global_min, pop_size=c.pop_size,
+                           cluster_size=c.cluster_size, max_gens=c.max_gen,
+                           max_no_success=c.max_no_success, run_id=c.run_id)
+
+    return 0
+
+
+def genetic_algorithm() -> None:
+    """
+    The main genetic algorithm
+    """
+    np.random.seed(241)
     np.seterr(divide='raise')
 
     # Provide file name
@@ -325,16 +377,16 @@ def genetic_algorithm() -> None:
 
     # Connect to database
     db_file = os.path.join(os.path.dirname(__file__), db_file)
-    db = ase.db.connect('./genetic_algorithm_results.db')
+    db = ase.db.connect(db_file)
 
     # Parse terminal input
     c = get_configuration(config_file)
 
-    
     # Output the run info to stdout
     config_info(c)
 
     # Lists for EPP plots
+    show_EPP = True
     lowest_energies = []
     highest_energies = []
     average_energies = []
@@ -367,22 +419,16 @@ def genetic_algorithm() -> None:
 
         # Mating - get new population
         children = mating(pop, pop_fitness, c.children_perc, c.mating_method)
-
-        # Mutating
+        # Mutating - get new mutants
         mutants = get_mutants(pop, c.cluster_radius, c.cluster_size)
 
         # Local minimisation and add to population
         newborns = children + mutants
-
         energies += optimise_local(newborns, c.calc, c.local_optimiser)
         pop += newborns
 
-        for i in range(len(newborns)):
-            too_close = np.isclose(
-                energies_min, energies[-(i + 1)], atol=c.dE_thr)
-            if not np.any(too_close):
-                local_min.append(newborns[i])
-                energies_min = np.append(energies_min, energies[i])
+        # Keep track of new local minima
+        local_min, energies_min = store_local_minima(newborns, energies, local_min, energies_min, c.dE_thr)
 
         # Natural selection
         pop, energies, pop_fitness = natural_selection_step(pop, energies, pop_fitness, c.pop_size, c.dE_thr, c.fitness_func)
@@ -394,11 +440,9 @@ def genetic_algorithm() -> None:
 
         # Store current best
         if energies[0] < best_min[-1].get_potential_energy():
-            best_min.append(pop[0])
             debug("New global minimum: ", energies[0])
-
+            best_min.append(pop[0])
             gen_no_success = 0  # This is success, so set to zero.
-
         else:
             gen_no_success += 1
 
@@ -409,19 +453,11 @@ def genetic_algorithm() -> None:
     debug("The evolution of the global minimum:")
     debug([cluster.get_potential_energy() for cluster in best_min])
 
-    for cluster in local_min:
-        global_min = False
-        if cluster == best_min[-1]:
-            global_min = True
-
-        last_id = db.write(cluster, global_min=global_min, pop_size=c.pop_size,
-                           cluster_size=c.cluster_size, max_gens=c.max_gen,
-                           max_no_success=c.max_no_success, run_id=c.run_id)
+    store_results_database(best_min[-1], local_min, db, c)
 
     # Show EPP plot if desired
-    show_EPP = True
     if show_EPP:
-        plot_EPP(lowest_energies, highest_energies, average_energies)
+        plot_EPP(lowest_energies, highest_energies, average_energies, c.run_id)
 
     return 0
 
