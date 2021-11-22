0ebf740c2612b547022ffdfdf8266517998b407d
donghwikim7 <93977784+donghwikim7@users.noreply.github.com>
2021-11-22 16:51:52 +0100

    Update employee_bee.py

--- abc/employee_bee.py
+++ abc/employee_bee.py
@@ -1,60 +1,69 @@
 #!/bin/python3
 import numpy as np
 
-from ase import Atoms
-from ase.calculators.lj import LennardJones
-from ase.optimize import LBFGS
-from ase.visualize import view
-from ase.io import write
-import ase.db
-
-from typing import List
-# from mating import mating
-# import mutators
-import argparse
 import artificial_bee_colony_algorithm
-import sys
-
 import random
-import math
 
 
+def employee_bee_func(pop, s_n, cluster_size, calc, local_optimiser):
+    """
+        employed bee discovers neighbor structure and creates new structure
+
+        pop: the given population
+        s_n: number of population
+        cluster_size: size of the cluster
+        calc: method of the calculator
+        local_optimiser: function which optimises locally
+        """
+    pop = employee_bee_mutation(pop, s_n, cluster_size, calc, local_optimiser, 3)
+
+    return pop
 
 
-def employee_bee_func(pop, Sn, cluster_size, calc, local_optimiser):
+def employee_bee_mutation(pop, s_n, cluster_size, calc, local_optimiser, mutation_n):
+    """
+            employed bee discovers neighbor structure and creates new structure
 
+            pop: the given population
+            s_n: number of population
+            cluster_size: size of the cluster
+            calc: method of the calculator
+            local_optimiser: function which optimises locally
+            mutation_n: eg trigonometric-> 3
+            """
     pop_copy = pop.copy()
     for cluster in pop_copy:
         cluster.calc = calc
     for i in range(len(pop)):
-        sum_E = 0
-        E_1 = 0
-        E_2 = 0
-        E_3 = 0
-        while sum_E == 0:
-            random_index = random.sample(range(0, Sn), 3)
-            while random_index[0] == i | random_index[1] == i | random_index[2] == i:
-                random_index = random.sample(range(0, Sn), 3)
-            E_1 = np.abs(pop_copy[0].get_potential_energy())
-            E_2 = np.abs(pop_copy[1].get_potential_energy())
-            E_3 = np.abs(pop_copy[2].get_potential_energy())
-            sum_E = (E_1 + E_2 + E_3)
+        sum_e = 0.0
+        e = np.array([])
+        random_index = random.sample(range(0, s_n), mutation_n)
+        while sum_e == 0:
+            while i in random_index :
+                random_index = random.sample(range(0, s_n), mutation_n)
+            for j in range(mutation_n):
+                e = np.append(e, np.abs(pop_copy[j].get_potential_energy()))
+            sum_e = np.sum(e)
 
-        p_1 = E_1 / sum_E
-        p_2 = E_2 / sum_E
-        p_3 = E_3 / sum_E
-        new_x = artificial_bee_colony_algorithm.generate_cluster_with_position((1.0 / 3.0) * (pop_copy[0].get_positions()
-                                                                  + pop_copy[1].get_positions() + pop_copy[
-                                                                      2].get_positions())
-                                                   + (p_2 - p_1) * (pop_copy[0].get_positions() - pop_copy[
-                1].get_positions())
-                                                   + (p_3 - p_2) * (pop_copy[1].get_positions() - pop_copy[
-                2].get_positions())
-                                                   + (p_1 - p_3) * (pop_copy[2].get_positions() - pop_copy[
-                0].get_positions()),
-                                                   cluster_size)
-        new_x = artificial_bee_colony_algorithm.optimise_local_each(new_x, calc, local_optimiser)
+        p = np.array([])
+        for j in range(mutation_n):
+            p = np.append(p, e[j] / sum_e)
+        new_x_position = 0.0
+        for j in range(mutation_n):
+            new_x_position += pop_copy[random_index[j]].get_positions()
+        new_x_position /= mutation_n
+
+        for j in range(mutation_n):
+            if j == mutation_n-1:
+                new_x_position += (p[0] - p[j]) * (pop_copy[random_index[j]].get_positions() -
+                                                   pop_copy[random_index[0]].get_positions())
+            else:
+                new_x_position += (p[j+1] - p[j]) * (pop_copy[random_index[j]].get_positions()
+                                                     - pop_copy[j+1].get_positions())
+
+        new_x = artificial_bee_colony_algorithm.optimise_local_each(
+            artificial_bee_colony_algorithm.generate_cluster_with_position(new_x_position, cluster_size),
+            calc, local_optimiser)
         if new_x.get_potential_energy() <= pop[i].get_potential_energy():
             pop[i] = new_x
-
     return pop
