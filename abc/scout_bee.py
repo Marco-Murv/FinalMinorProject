ab232faf9a2ca9d19fe7881270b55ccf1b0343f4
Patrick Hibbs <p.j.hibbs@student.tudelft.nl>
2021-11-23 01:11:53 +0100

    Newly added clusters are locally optimized before adding them to the total population

--- abc/scout_bee.py
+++ abc/scout_bee.py
@@ -4,7 +4,7 @@ import sys
 import artificial_bee_colony_algorithm
 
 
-def scout_bee_func(pop, Sn, cluster_size, cluster_radius, calc):
+def scout_bee_func(pop, Sn, cluster_size, cluster_radius, calc, local_optimiser):
     minimal_pe = sys.maxsize  # lowest potential energy
 
     for cluster in pop:
@@ -13,12 +13,13 @@ def scout_bee_func(pop, Sn, cluster_size, cluster_radius, calc):
 
     new_pop = []
     for cluster in pop:
-        if (cluster.get_potential_energy() / minimal_pe) >= 0.4:
+        if (cluster.get_potential_energy() / minimal_pe) >= 0.8:
             new_pop.append(cluster)
 
     if len(pop) != len(new_pop):  # replace the old removed clusters with new clusters
         # if n clusters were removed, then n new clusters are added
         new_clusters = artificial_bee_colony_algorithm.generate_population(Sn, cluster_size, cluster_radius)[:len(pop)-len(new_pop)]
+        new_clusters = artificial_bee_colony_algorithm.optimise_local(new_clusters, calc, local_optimiser)
 
         for cluster in new_clusters:
             cluster.calc = calc
