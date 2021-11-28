51cbb1c168e4defa00608710bc37d14f14ab4824
Simon Van Hulle <simonvanhulle01@gmail.com>
2021-11-28 11:20:40 +0100

    Reworked chunks to be sent to cpus

--- genetic_algorithm/ga_distributed.py
+++ genetic_algorithm/ga_distributed.py
@@ -10,17 +10,18 @@ import inspect
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
-
-import process_data
+sys.path.insert(0, parentdir)
 
 import ase
+
+import process_data
 import genetic_algorithm as ga
+from mating import mating
+
 from genetic_algorithm import config_info, debug, fitness, generate_population
 from genetic_algorithm import get_configuration, natural_selection_step
 from genetic_algorithm import optimise_local, fitness, get_mutants
-from genetic_algorithm import store_local_minima, store_results_database
-from mating import mating
+from genetic_algorithm import store_results_database
 
 import numpy as np
 from mpi4py import MPI
@@ -40,7 +41,6 @@ def ga_distributed():
     """
     Main genetic algorithm (distributed)
     """
-    np.random.seed(241)
     np.seterr(divide='raise')
 
     # Initialising MPI
@@ -54,6 +54,10 @@ def ga_distributed():
     # Parse possible terminal input and yaml file.
     c = get_configuration(config_file)
 
+    # Start timer
+    if rank == 0:
+        ga_start_time = MPI.Wtime()
+
     # =========================================================================
     # Initial population
     # =========================================================================
@@ -64,22 +68,15 @@ def ga_distributed():
     if rank == 0:
         config_info(c)
 
-        # Provide file name
-        db_file = "genetic_algorithm_results.db"
-
-        # Connect to database
-        db_file = os.path.join(os.path.dirname(__file__), db_file)
-        db = ase.db.connect(db_file)
-
         # Create initial population
         pop = generate_population(c.pop_size, c.cluster_size, c.cluster_radius)
 
         # FIX: Make this parallel already
         energies = optimise_local(pop, c.calc, c.local_optimiser)
+
         # Keep track of global minima. Initialised with random cluster
         best_min = [pop[0]]
         local_min = [pop[0]]
-        energies_min = np.array(pop[0].get_potential_energy())
 
     # =========================================================================
     # Main loop
@@ -95,25 +92,22 @@ def ga_distributed():
 
         # Broadcast initial population
         pop = comm.bcast(pop, root=0)
-        # TODO: use Bcast instead (numpy)
-        energies = comm.bcast(energies, root=0)
+        energies = comm.bcast(energies, root=0) # TODO: use Bcast instead?
 
-
-        pop_fitness = fitness(energies, c.fitness_func)
         # Mating - get new population
-        children = mating(pop, pop_fitness, c.children_perc/num_procs, c.mating_method)
+        pop_fitness = fitness(energies, c.fitness_func)
+        children = mating(pop, pop_fitness, c.children_perc /
+                          num_procs, c.mating_method)
 
         # Define sub-populaiton on every rank (only for mutating)
         chunk = len(pop) // num_procs  # TODO:
-        sub_pop = pop[rank * chunk:(rank + 1) * chunk]
+        sub_pop = pop[rank * chunk:(rank + 1) * chunk] or pop[-2:]
 
         # Mutating - get new mutants
         mutants = get_mutants(sub_pop, c.cluster_radius, c.cluster_size)
 
         # Local minimisation and add to population
         newborns = children + mutants
-
-        debug(f"\tRank {rank:2d} - Local optimisation of {len(newborns)} newb")
         new_energies = optimise_local(newborns, c.calc, c.local_optimiser)
 
         newborns = comm.gather(newborns, root=0)
@@ -126,14 +120,12 @@ def ga_distributed():
             pop += newborns
             energies.extend(new_energies)
 
-            # # Keep track of new local minima
-            # local_min, energies_min = store_local_minima(
-            #     newborns, energies, local_min, energies_min, c.dE_thr)
+            # Add new local minima to the list
+            local_min += newborns
 
             # Natural Selection
-            # debug(f"\tRank {rank:2d} - Natural Selection")
-            pop, energies= natural_selection_step(
-                pop, energies, c.pop_size, c.dE_thr, c.fitness_func)
+            pop, energies = natural_selection_step(pop, energies, c.pop_size,
+                                                   c.dE_thr)
 
             # Store current best
             if energies[0] < best_min[-1].get_potential_energy():
@@ -148,27 +140,27 @@ def ga_distributed():
             # Check if done
             done = gen_no_success > c.max_no_success or gen > c.max_gen
 
-
-
         gen = comm.bcast(gen, root=0)
         gen_no_success = comm.bcast(gen_no_success, root=0)
         done = comm.bcast(done, root=0)
 
     if rank == 0:
-        # Store / 
-        process_data.print_stat(local_min)
-        debug(f"Found {len(local_min)} local minima in total.")
-        debug("The evolution of the global minimum:")
-        debug([cluster.get_potential_energy() for cluster in best_min])
+        # Stop timer
+        ga_time = MPI.Wtime() - ga_start_time
+        print(f"\nga_distributed took {ga_time} seconds to execute")
 
+        # Process and report local minima
+        local_min = process_data.select_local_minima(local_min)
+        process_data.print_stats(local_min)
+
+        # Store in Database
+        db_file = os.path.join(os.path.dirname(__file__), c.db_file)
+        db = ase.db.connect(db_file)
         store_results_database(best_min[-1], local_min, db, c)
 
     return 0
 
 
 if __name__ == "__main__":
-    start = MPI.Wtime()
-    ga_distributed()
-    wt = MPI.Wtime() - start
 
-    print(f"\nga_distributed took {wt} seconds to execute")
+    ga_distributed()
