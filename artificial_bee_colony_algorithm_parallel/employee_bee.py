bb391324b6ecedbd56880b86f01948df241336e8
donghwikim7 <93977784+donghwikim7@users.noreply.github.com>
2021-11-30 00:42:49 +0100

    Update employee_bee.py

--- artificial_bee_colony_algorithm_parallel/employee_bee.py
+++ artificial_bee_colony_algorithm_parallel/employee_bee.py
@@ -1,4 +1,6 @@
 #!/bin/python3
+import time
+
 import numpy as np
 
 import artificial_bee_colony_algorithm
@@ -7,7 +9,8 @@ from mpi4py import MPI
 import math
 
 
-def employee_bee_func(pop, s_n, cluster_size, calc, local_optimiser, comm, rank, total_p, is_parallel):
+def employee_bee_func(pop, s_n, cluster_size, calc, local_optimiser, comm, rank, total_p, is_parallel,
+                      eb_mutation_size):
     """
         employed bee discovers neighbor structure and creates new structure
 
@@ -16,86 +19,70 @@ def employee_bee_func(pop, s_n, cluster_size, calc, local_optimiser, comm, rank,
         cluster_size: size of the cluster
         calc: method of the calculator
         local_optimiser: function which optimises locally
+        eb_mutation_size: eg trigonometric-> 3
         """
 
-    mutation_n = 3
-
     if is_parallel == 1:
-        if rank == 0:
-            buffer_size = math.floor(s_n/(total_p-1))
-            #print(buffer_size)
-            for i in range(total_p-1):
-
-                crr = i + 1
-                if crr != total_p-1 :
-                    comm.send([(crr-1)*buffer_size,crr*buffer_size ], dest=crr)
-                else:
-                    comm.send([(crr - 1) *buffer_size, s_n], dest=crr)
-
-            final_pop = []
-            for i in range(total_p - 1):
-                rec = comm.recv(source=MPI.ANY_SOURCE)
-                #print(rec[0])
-                final_pop = final_pop+rec
-
-            #print(len(final_pop))
-            return final_pop
+        buffer_size = math.floor(s_n / total_p)
+        start_i = rank * buffer_size
+        if rank == total_p - 1:
+            end_i = s_n
         else:
-            pop_final_re = []
-            index_start_end = comm.recv(source=0)
-            #print(index_start_end [0])
-            #print(index_start_end[1])
-            for i in range(index_start_end[0], index_start_end[1]):
-                sum_e = 0.0
-                e = np.array([])
-                random_index = random.sample(range(0, s_n), mutation_n)
-                while sum_e == 0:
-                    while i in random_index:
-                        random_index = random.sample(range(0, s_n), mutation_n)
-                    for j in range(mutation_n):
-                        e = np.append(e, np.abs(pop[random_index[j]].get_potential_energy()))
-                    sum_e = np.sum(e)
+            end_i = (rank + 1) * buffer_size
+        pop_final = []
+        for i in range(start_i, end_i):
+            sum_e = 0.0
+            # array to save the atom to mutate
+            e = np.array([])
+            random_index = 0
+            # if sum is 0, it leads to n/0 error in the later step
+            while sum_e == 0:
+                # select random fpr the mutation
+                random_index = random.sample([x for x in range(s_n) if x != i], eb_mutation_size)
+                for j in range(eb_mutation_size):
+                    e = np.append(e, np.abs(pop[random_index[j]].get_potential_energy()))
+                sum_e = np.sum(e)
 
-                p = np.array([])
-                for j in range(mutation_n):
-                    p = np.append(p, e[j] / sum_e)
-                new_x_position = 0.0
-                for j in range(mutation_n):
-                    new_x_position += pop[random_index[j]].get_positions()
-                new_x_position /= mutation_n
+            # p =  |e[m]| / (|e[1]|+|e[1]|+ |e[1]|) where m = 1, 2, 3
+            p = np.array([])
+            for j in range(eb_mutation_size):
+                p = np.append(p, e[j] / sum_e)
 
-                for j in range(mutation_n):
-                    if j == mutation_n - 1:
-                        new_x_position += (p[0] - p[j]) * (pop[random_index[j]].get_positions() -
-                                                           pop[random_index[0]].get_positions())
-                    else:
-                        new_x_position += (p[j + 1] - p[j]) * (pop[random_index[j]].get_positions()
-                                                               - pop[j + 1].get_positions())
+            # get position of atoms through mutation
+            new_atoms_position = 0.0
+            for j in range(eb_mutation_size):
+                new_atoms_position += pop[random_index[j]].get_positions()
+            new_atoms_position /= eb_mutation_size
 
-                new_x = artificial_bee_colony_algorithm.optimise_local_each(
-                    artificial_bee_colony_algorithm.generate_cluster_with_position(new_x_position, cluster_size),
-                    calc, local_optimiser)
-                if new_x.get_potential_energy() <= pop[i].get_potential_energy():
-                    pop_final_re.append(new_x)
-                    #print(new_x)
-                else:
-                    pop_final_re.append( pop[i])
+            for j in range(eb_mutation_size - 1):
+                new_atoms_position += (p[j + 1] - p[j]) * (pop[random_index[j]].get_positions()
+                                                           - pop[j + 1].get_positions())
+            new_atoms_position += (p[0] - p[eb_mutation_size - 1]) * (
+                        pop[random_index[eb_mutation_size - 1]].get_positions() -
+                        pop[random_index[0]].get_positions())
 
-           # print(pop_final_re[0])
-            comm.isend(pop_final_re, dest=0)
-            return pop
+            # create new Atoms with new positions and locally optimise
+            new_x = artificial_bee_colony_algorithm.optimise_local_each(
+                artificial_bee_colony_algorithm.generate_cluster_with_position(new_atoms_position, cluster_size),
+                calc, local_optimiser)
 
+            # if new Atoms has better potential energy than current Atoms replace, otherwise keep
+            if new_x.get_potential_energy() <= pop[i].get_potential_energy():
+                pop_final.append(new_x)
+            else:
+                pop_final.append(pop[i])
 
-
-
+        pop_final = comm.gather(pop_final, root=0)
+        if rank == 0:
+            return sum(pop_final, [])
+        else:
+            return None
 
     else:
-        print("dasdasd")
-        if rank ==0:
-            pop = employee_bee_mutation(pop, s_n, cluster_size, calc, local_optimiser, 3)
+        if rank == 0:
+            # not parallelized version
+            pop = employee_bee_mutation(pop, s_n, cluster_size, calc, local_optimiser, eb_mutation_size)
         return pop
-
-
 
 
 def employee_bee_mutation(pop, s_n, cluster_size, calc, local_optimiser, mutation_n):
@@ -117,7 +104,7 @@ def employee_bee_mutation(pop, s_n, cluster_size, calc, local_optimiser, mutatio
         e = np.array([])
         random_index = random.sample(range(0, s_n), mutation_n)
         while sum_e == 0:
-            while i in random_index :
+            while i in random_index:
                 random_index = random.sample(range(0, s_n), mutation_n)
             for j in range(mutation_n):
                 e = np.append(e, np.abs(pop_copy[random_index[j]].get_potential_energy()))
@@ -127,17 +114,18 @@ def employee_bee_mutation(pop, s_n, cluster_size, calc, local_optimiser, mutatio
         for j in range(mutation_n):
             p = np.append(p, e[j] / sum_e)
         new_x_position = 0.0
+
         for j in range(mutation_n):
             new_x_position += pop_copy[random_index[j]].get_positions()
         new_x_position /= mutation_n
 
         for j in range(mutation_n):
-            if j == mutation_n-1:
+            if j == mutation_n - 1:
                 new_x_position += (p[0] - p[j]) * (pop_copy[random_index[j]].get_positions() -
                                                    pop_copy[random_index[0]].get_positions())
             else:
-                new_x_position += (p[j+1] - p[j]) * (pop_copy[random_index[j]].get_positions()
-                                                     - pop_copy[j+1].get_positions())
+                new_x_position += (p[j + 1] - p[j]) * (pop_copy[random_index[j]].get_positions()
+                                                       - pop_copy[j + 1].get_positions())
 
         new_x = artificial_bee_colony_algorithm.optimise_local_each(
             artificial_bee_colony_algorithm.generate_cluster_with_position(new_x_position, cluster_size),
