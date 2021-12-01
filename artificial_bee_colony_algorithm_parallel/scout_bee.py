63bdff220dd3b241dec487af2449bba77b501bfd
Patrick Hibbs <p.j.hibbs@student.tudelft.nl>
2021-12-01 02:25:01 +0100

    Parallelizing scout bee

--- artificial_bee_colony_algorithm_parallel/scout_bee.py
+++ artificial_bee_colony_algorithm_parallel/scout_bee.py
@@ -3,21 +3,31 @@
 import sys
 import artificial_bee_colony_algorithm
 import numpy as np
+from mpi4py import MPI
 
 
 def scout_bee_func(pop, s_n, cluster_size, cluster_radius, calc, local_optimiser):
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
     minimal_pe = sys.maxsize  # lowest potential energy
 
     for cluster in pop:
         pe = cluster.get_potential_energy()
         if pe < minimal_pe: minimal_pe = pe
 
-    new_pop = []
+    splitted_pop = split(pop, comm.Get_size()) # divides the array into n parts to divide over processors
+    pop = comm.scatter(splitted_pop, root=0)
+    result = []
     for cluster in pop:
         if (cluster.get_potential_energy() / minimal_pe) >= 0.65:
             if cluster.get_potential_energy() < 0:
-                new_pop.append(cluster)
-    #print(len(pop) - len(new_pop))
+                result.append(cluster)
+
+    new_pop = comm.gather(result, root=0) # gather the result from all processes to master
+    new_pop = [item for sublist in new_pop for item in sublist] # flatten array to 1D array
+
     energy_diff = 0.04
     energies = [cluster.get_potential_energy() for cluster in new_pop]
     combined = list(zip(new_pop, energies))
@@ -26,15 +36,31 @@ def scout_bee_func(pop, s_n, cluster_size, cluster_radius, calc, local_optimiser
     ind = np.argsort(minima[:, -1])
     sorted_clusters = minima[ind]
 
+    # This is the parallel part of the below for loop. Master-worker pattern.
+    local_minima = [sorted_clusters[0]]
+    if rank == 0:
+        for i in range(sorted_clusters.shape[0]):
+            comm.send(sorted_clusters[i, 1], dest=i, tag=i)
+            local_minima = comm.recv(source=i)
+    else:
+        data = comm.recv(source=0, tag=rank)
+
+        energy = data
+        if np.abs(local_minima[-1][1] - energy) > energy_diff:
+            local_minima.append(sorted_clusters[rank])
+
+        comm.send(local_minima, dest=0)
+
+    """ This is the serial version of the for loop
     local_minima = [sorted_clusters[0]]
     for i in range(sorted_clusters.shape[0]):
         energy = sorted_clusters[i, 1]
         if np.abs(local_minima[-1][1] - energy) > energy_diff:
             local_minima.append(sorted_clusters[i])
+    """
 
     new_pop = [cluster for [cluster, energy] in local_minima]
     if len(pop) != len(new_pop):  # replace the old removed clusters with new clusters
-        #print(len(pop) - len(new_pop))
         # if n clusters were removed, then n new clusters are added
         new_clusters = artificial_bee_colony_algorithm.generate_population(s_n, cluster_size, cluster_radius)[
                        :len(pop) - len(new_pop)]
@@ -42,3 +68,8 @@ def scout_bee_func(pop, s_n, cluster_size, cluster_radius, calc, local_optimiser
         new_pop += new_clusters
 
     return new_pop
+
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return list(a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
